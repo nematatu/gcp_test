2053a07c69817a4ed5a28080fe9936f26e6f571b
nematatu <nematatu1003@gmail.com>
2024-01-21 03:38:38 +0900

    hello

--- frontend/main.py
+++ frontend/main.py
@@ -3,9 +3,6 @@ import requests
 
 st.title('Hello!')
 API_URL="https://gcp-test.onrender.com"
-res=requests.get(API_URL+"/"+"hello")
+res=requests.get("https://gcp-test.onrender.com/hello")
 records=res.json()
-st.subheader(records[message])
-res=requests.get(API_URL+"/"+"world")
-records=res.json()
-st.subheader(records[message])
+st.write(records)
