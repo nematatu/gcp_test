7f8782290a9be77ec4474f77af8887c8e6bfac40
nematatu <nematatu1003@gmail.com>
2024-01-21 03:31:32 +0900

    hello world

--- frontend/main.py
+++ frontend/main.py
@@ -2,6 +2,10 @@ import streamlit as st
 import requests
 
 st.title('Hello!')
-res=requests.get(API_URL)
+API_URL="https://gcp-test.onrender.com"
+res=requests.get(API_URL+"/"+"hello")
 records=res.json()
-st.subheader(records)
+st.subheader(records[message])
+res=requests.get(API_URL+"/"+"world")
+records=res.json()
+st.subheader(records[message])
